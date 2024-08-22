8291309f2f8ea098d4ebff4d7c1a6060ba413cd8
khinelynnhtwan <khinelynnhtwan@gmail.com>
2024-08-22 15:47:01 +0630

    add loges

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   dark: ['class'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Proxima Nova"', 'Arial'],
+        'sora': ['Sora', 'Padauk', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+        'moderustic': ["Moderustic", 'sans-serif'],
+        'ubuntu': ['Ubuntu', 'sans-serif'],
+        'josefin': ["Josefin Sans", 'sans-serif']
+      },
+
+      colors: {
+        'text': '#110909',
+        'background': '#ffffff',
+        'primary': '#2e2e2e',
+        'secondary': '#cfcfcf',
+        'accent': '#7ca3bb',
+       },
+       
+       
+    },
   },
   plugins: [],
+
 }

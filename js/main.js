4432b90ee77535aa4789dc9451966bb77664c3c8
khinelynnhtwan <khinelynnhtwan@gmail.com>
2024-10-08 15:40:45 +0630

    ui_modify

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+const home = document.querySelector('#homeTag');
+home.className = "text-red-500 font-medium border-b-8";
+console.log(home);
+
+// homeTagHandler = () => {
+//     home.className = 'hover:border';
+// };
+
+// home.addEventListener('click', homeTagHandler);
